cf9345995b8b00ed041ac29efef9c1ccf45022ab
Nick <sshedl22@gmail.com>
2019-05-14 10:52:52 +0300

    almost final.Need debug and typedef.

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -10,68 +10,12 @@
 using namespace std;
 
 int main() {
-/*    multimap <int,int> myFirstMap = {{ 2, 37 },
-                                   { 4, 40 },
-                                   { 3, 15 },
-                                   { 5, 15 }};
-    myFirstMap.insert(pair<int,int>(5,100));
-    for (const auto & i : myFirstMap) {
-        cout << i.first << ":" << i.second << " ";
+    coolIterSet As = {1, 2, 3, 8, 3, 9, 1, 2};
+    As.showSeq();
+    coolIterSet Bs = {1, 2};
+    Bs.showSeq();
+    As.excl(Bs).showSeq();
 
-    }
-    cout << endl;
-    multimap <int,int> mySecondMap = {{ 2, 37 },
-                                { 4, 40 },
-                                { 3, 15 },
-                                };
-    for (const auto & i : mySecondMap) {
-        cout << i.first << ":" << i.second << " ";
-
-    }
-    cout << endl;
-    multimap <int,int> finalMap = myFirstMap;
-    finalMap.insert(mySecondMap.cbegin(),mySecondMap.cend());
-
-    for (const auto & i : finalMap) {
-        cout << i.first << ":" << i.second << " ";
-
-    }
-    vector<int> vec1 = {1,2,3,4,5,6,2,4,1,6,2,13,5,2,14};
-    vector<int> vec2 = {6,2};
-    __gnu_cxx::__normal_iterator<const int *, vector<int, allocator<int>>> it1;
-    while(true) {
-        it1 = search(vec1.cbegin(), vec1.cend(), vec2.cbegin(), vec2.cend());
-        if(it1 != vec1.cend())
-        vec1.erase(it1, it1 + vec2.size());
-        else break;
-    }
-    for(auto t : vec1)
-        cout << t << " ";
-    cout << endl;*/
-    /* STLseq A = {1, 2, 3, 4, 5, 10, 15};
-     STLseq B = {5, 3, 1, 6, 100};
-     A.contact(B).showSeq();
-     A.showMap();
-    */
-    multiset<int> A;
-    list<decltype(A.begin())> itList;
-    vector<int> data = {1,3,8,1,5,3,7};
-
-    for (auto &i : data){
-        itList.push_back(A.insert(i));
-    }
-    for(auto &i : itList)
-       cout << *i << " ";
-    for (auto & i : A){
-
-    }
     ///~~~~~~~~~~~~~~~~~~~~~~~~~~~
-   /* unordered_multiset<int> C;
-    C.insert({1, 2, 3, 4, 3, 6, 1, 9, 3, 1});
-    for (auto &i : C) cout << i << " ";
-    cout << endl;
-    C.insert({5, 1, 7, 9, 2});
-    for (auto &i : C) cout << i << " ";
-    cout << endl;*/
     return 0;
 }
--- Lab6/source/UltimateClass.hpp
+++ Lab6/source/UltimateClass.hpp
@@ -5,31 +5,26 @@
 #define LAB6_ULTIMATECLASS_HPP
 using namespace std;
 
-class STLseq {
+bool pred(const set<int>::iterator &a, const set<int>::iterator &b) {
+    return *a == *b;
+}
+
+class coolIterSet {
 public:
-    STLseq(char A, unsigned n) : name('A') {
+    explicit coolIterSet(unsigned n) : name(code_name++) {
         for (int i = 0; i < n; ++i) {
             if (rand() % 2) {
-                variety.insert(pair<unsigned, int>(i, rand() % 100));
-                seq.push_back(i);
-                bigKey = i;
+                seq.push_back(variety.insert(rand() % 100));
             }
         }
     };
 
-    STLseq &mul(unsigned n) {
-        auto tmp = variety;
-        for (int i = 0; i < n; ++i) {
-            variety.insert(tmp.cbegin(), tmp.cend());
-            seq.insert(seq.cend(), seq.cbegin(), seq.cend());
-        }
-        return *this;
-    }
+    coolIterSet(multiset<int> copySet, list<multiset<int>::iterator> copyList) : variety(move(copySet)),
+                                                                                 seq(move(copyList)),
+                                                                                 name(code_name++) {};
 
     void push_back(int val) {
-        bigKey++;
-        variety.insert(pair<unsigned, int>(bigKey, val));
-        seq.push_back(bigKey);
+        seq.push_back(variety.insert(val));
     }
 
     void add_values(initializer_list<int> list) {
@@ -37,43 +32,114 @@ public:
             push_back(a);
     }
 
-    STLseq(initializer_list<int> list) : name('0'), bigKey(-1) {
+    coolIterSet(initializer_list<int> list) : name('0') {
         for (auto &a : list)
             push_back(a);
     }
 
     void showSeq() {
         for (auto &i : seq) {
-            cout << variety.find(i)->second << " ";
+            cout << *i << " ";
         }
         cout << endl;
     }
 
-    void showMap();
+    coolIterSet &mul(unsigned n);
 
-    STLseq &contact(STLseq &another);
+    coolIterSet &contact(coolIterSet &another);
 
+    coolIterSet &excl(coolIterSet &another);
+
+    coolIterSet operator&(coolIterSet &another);
+
+    coolIterSet operator|(coolIterSet &another);
+
+    coolIterSet operator^(coolIterSet &another);
+
+    coolIterSet operator~();
 private:
-    char name;
-    unsigned bigKey;
-    multimap<unsigned, int> variety;
-    vector<unsigned> seq;
+    int name;
+    static int code_name;
+    static const int MAX_INT = 100;
+    multiset<int> variety;
+    list<multiset<int>::iterator> seq;
 };
 
-void STLseq::showMap() {
-    for (auto &i : variety) {
-        cout << i.first << ":" << i.second << " ";
+
+coolIterSet &coolIterSet::contact(coolIterSet &another) {
+    for (auto &i : another.seq) {
+        push_back(*i);
     }
-    cout << endl;
-}
-
-STLseq &STLseq::contact(STLseq &another) {
-    bigKey = max(bigKey, another.bigKey);
-    for (auto &i : another.variety) {
-        push_back(i.second);
-
-    }// add no insetrt
     return *this;
 }
 
+coolIterSet &coolIterSet::mul(unsigned n) {
+    auto tmp = seq;
+    for (int i = 0; i < n; ++i) {
+        for (auto &it : tmp)
+            push_back(*it);
+    }
+    return *this;
+}
+
+coolIterSet &coolIterSet::excl(coolIterSet &another) {
+    unsigned size = another.seq.size();
+    while (true) {
+        auto it = search(seq.begin(), seq.end(), another.seq.begin(), another.seq.end(), pred);
+        if (it != seq.end()) {
+            for (int i = 0; i < size; ++i) {
+                variety.erase(*it);
+                it = seq.erase(it);
+            }
+        } else break;
+    }
+    return *this;
+}
+
+coolIterSet coolIterSet::operator^(coolIterSet &another) {
+
+    multiset<int> newObj;
+    list<multiset<int>::iterator> newIts;
+    set_symmetric_difference(variety.begin(), variety.end(), another.variety.begin(), another.variety.end(),
+                             back_inserter(newObj));
+    for (auto it = newObj.begin(); it != newObj.end(); it++) {
+        newIts.push_back(it);
+    }
+    return coolIterSet(newObj, newIts);
+}
+
+coolIterSet coolIterSet::operator&(coolIterSet &another) {
+    multiset<int> newObj;
+    list<multiset<int>::iterator> newIts;
+    set_intersection(variety.begin(), variety.end(), another.variety.begin(), another.variety.end(),
+                     back_inserter(newObj));
+    for (auto it = newObj.begin(); it != newObj.end(); it++) {
+        newIts.push_back(it);
+    }
+    return coolIterSet(newObj, newIts);
+}
+
+coolIterSet coolIterSet::operator~() {
+    multiset<int> full;
+    for (int i = 0; i < MAX_INT + 1; ++i) {
+        full.insert(i);
+    }
+    multiset<int> newObj;
+    list<multiset<int>::iterator> newIts;
+    set_difference(full.begin(), full.end(), variety.begin(), variety.end(), back_inserter(newObj));
+    for (auto it = newObj.begin(); it != newObj.end(); it++) {
+        newIts.push_back(it);
+    }
+    return coolIterSet(newObj, newIts);
+}
+
+coolIterSet coolIterSet::operator|(coolIterSet &another) {
+    multiset<int> newObj;
+    list<multiset<int>::iterator> newIts;
+    set_union(variety.begin(), variety.end(), another.variety.begin(), another.variety.end(), back_inserter(newObj));
+    for (auto it = newObj.begin(); it != newObj.end(); it++) {
+        newIts.push_back(it);
+    }
+    return coolIterSet(newObj, newIts);
+}
 #endif //LAB6_ULTIMATECLASS_HPP
